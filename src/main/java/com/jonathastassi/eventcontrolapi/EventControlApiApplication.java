92d704e85a99ea20748c0680216b1aa6fac999ac
Jonathas Tassi <jonathastassi@hotmail.com>
2023-11-14 17:50:13 -0300

    First commit

---
+++ src/main/java/com/jonathastassi/eventcontrolapi/EventControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.jonathastassi.eventcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathastassi/eventcontrolapi/EventControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathastassi.eventcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
